b0af596834222b19f265f0aee360e077841893a8
yasuhiroki <yasuhiroki.duck@gmail.com>
2016-07-31 08:50:12 +0900

    Add client.py include API request modules.

--- aozoracli/books.py
+++ aozoracli/books.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+
 import requests
+
+import aozoracli.client
 
 def main():
     print("Start aozora command line tool")
-    r = requests.get("http://153.127.202.91/api/v0.1/books/")
-    print(r.json())
+    books = aozoracli.client.get_books().json()
+    print(books)
--- aozoracli/main.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def main():
-    print("Start aozora command line tool")
-    r = requests.get("http://153.127.202.91/api/v0.1/books/")
-    print(r.json())
