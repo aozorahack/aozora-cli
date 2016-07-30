a4752e64253a786b0f797e2e9d4ed39430b814f9
yasuhiroki <yasuhiroki.duck@gmail.com>
2016-07-30 16:55:55 +0900

    Fix port.

--- aozoracli/main.py
+++ aozoracli/main.py
@@ -2,5 +2,5 @@ import requests
 
 def main():
     print("Start aozora command line tool")
-    r = requests.get("http://153.127.202.91:5000/api/v0.1/books/")
+    r = requests.get("http://153.127.202.91/api/v0.1/books/")
     print(r.json())
